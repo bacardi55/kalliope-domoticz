e4db81647197c243e0133ad94a9d36620ba669f6
Raphael Khaiat <raphael@khaiat.org>
2017-03-14 22:57:33 +0100

    add possibility to get device data

--- domoticz.py
+++ domoticz.py
@@ -1,12 +1,7 @@
 import logging
+import imp
 
 from kalliope.core.NeuronModule import NeuronModule, InvalidParameterException
-
-#import sys
-#import os
-#sys.path.append(os.getcwd())
-#from . import Pymoticz
-
 
 logging.basicConfig()
 logger = logging.getLogger("kalliope")
@@ -17,18 +12,60 @@ class Domoticz (NeuronModule):
 
         # Get parameters form the neuron
         self.configuration = {
-            'host': kwargs.get('host', None)
+            'action': kwargs.get('action', None),
+            'host': kwargs.get('host', None),
+            # Because Kalliope receive arguments as string
+            'ssl_verify': False if kwargs.get('ssl_verify', True) == 'False' else True,
+            'device': kwargs.get('device', None)
         }
+        logger.debug(self.configuration)
 
-        message = {}
+        message = {
+            "status": "KO",
+            "devices": []
+        }
 
         # Check parameters:
         if self._is_parameters_ok():
-            p = Pymoticz(self.configuration['host'])
+            self.init_pymoticz()
+            if self.configuration['action'] == 'get_device':
+                message['devices'] = self._get_device(self.configuration['device'])
+                message['status'] = "OK"
+                logger.debug(message['devices'])
+                logger.debug(message['devices'][0]['Temp'])
+            elif self.configuration['action'] == 'get_scene':
+                pass
+            elif self.configuration['action'] == 'set_scene':
+                pass
+            elif self.configuration['action'] == 'set_scene_on':
+                pass
+            elif self.configuration['action'] == 'set_scene_off':
+                pass
+            elif self.configuration['action'] == 'set_light_on':
+                pass
+            elif self.configuration['action'] == 'set_light_off':
+                pass
 
-        message['p'] = p
-        #self.say(message)
+        self.say(message)
 
+    def init_pymoticz(self):
+        pymoticzFile = imp.load_source("Pymoticz", "resources/neurons/domoticz/pymoticz.py")
+        self.p = pymoticzFile.Pymoticz(self.configuration['host'], ssl_verify=self.configuration['ssl_verify'])
+
+    def _get_device(self, device_id=None):
+        devices = []
+        #devices = self.p.get_device
+        if device_id is None:
+            response = self.p.list()
+            devices = response['result']
+        else:
+            devices = [self.p.get_device(device_id)]
+
+        #logger.debug(devices)
+        return devices
+
+    def _is_response_ok(self, response):
+        return True if "status" in response and response['status'] == "OK" else False
 
     def _is_parameters_ok(self):
         """
